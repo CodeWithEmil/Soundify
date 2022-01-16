f989b765b9d7d6c6ba397c2b57a2cd2d41bef915
Emilio <emilionajera23@gmail.com>
2022-01-16 14:24:24 -0600

    I don't really know what I just did. It somehow 'works', but only when you change something while executing the code. I hate my life.

--- src/components/sounds/button/button.js
+++ src/components/sounds/button/button.js
@@ -11,6 +11,13 @@ class Button extends Component {
         this.sound = this.props.sound;
         this.audio = this.props.audio;
 
+        //Song selection
+            this.one = this.props.one;
+            this.two = this.props.two;
+            this.three = this.props.three;
+
+        this.callMethod = this.props.method;
+
         //Included elements
         this.container = this.props.container;
 
@@ -22,11 +29,17 @@ class Button extends Component {
 
         //State
         this.state = {
-            playing: true
+            playing: true,
+            playBeginning: true,
+            decision: false,
+            counter: 0
         }
 
         //Executing methods
         window.addEventListener('scroll', this.styling);
+        console.log(this.one)
+        console.log(this.two)
+        console.log(this.three)
     }
 
     //Some styles when we scroll the page
@@ -73,7 +86,17 @@ class Button extends Component {
 
         //Displaying wether if it is playing or not
         console.log("Playing audio? " + this.state.playing);
-        this.buttonContainer.current.classList.toggle("button--container--clicked");
+        
+        //Changing styles
+        if(this.state.counter === 1) {
+            if (!this.state.decision) {
+                this.buttonContainer.current.classList.add("button--container--clicked");
+            } else if(this.state.decision) {
+                this.buttonContainer.current.classList.remove("button--container--clicked");
+            }
+        } else {
+            this.buttonContainer.current.classList.toggle("button--container--clicked");
+        }
 
         //Playing/pausing the audio
         if (this.audio === null || this.audio === "" || this.audio === undefined) {
@@ -148,12 +171,104 @@ class Button extends Component {
 
         //Changing the State's values
         this.setState({
-            playing: !this.state.playing
+            playing: !this.state.playing,
+            playingBeginning: !this.state.playingBeginning,
+            decision: !this.state.decision
         })
         
         //Displaying a Toast (notification)
         toast.remove();
         return this.notif();
+    }
+
+    //Basically playPause() but adapted to when random is clicked
+    beginning = e => {
+        let array = [this.one, this.two, this.three];
+        console.log(array)
+
+        for(var i = 0; i < array.length; i++) {
+            if (this.audio === "" && (this.one === this.name || this.two === this.name || this.three === this.name)) {
+                //window.alert("Error 001: Source not found.");
+                this.buttonContainer.current.classList.remove("button--container--clicked");
+    
+                this.notif = () => toast.error(`Not found: '${this.name}'`, {
+                //Custom icon
+                    icon: "😞",
+            
+                    //Colors
+                    iconTheme: {
+                        primary: '#24292f',
+                        secondary: '#f9fafb',
+                    },
+                    /*style: {
+                        "box-shadow": "rgba(17, 12, 46, 0.041) 0 48px 100px 0"
+                    },*/
+            
+                    //Id
+                    id: "notif"
+                });
+            } else if(this.one === this.name || this.two === this.name || this.three === this.name) {
+                this.audioElement.current.play();
+    
+                this.notif = () => toast.success(`Playing '${this.name}'`, {
+                    //Custom icon
+                    icon: "🥑", //😄 , 🍀 , 🥑
+            
+                    //Colors
+                    iconTheme: {
+                        primary: '#24292f',
+                        secondary: '#f9fafb',
+                      },
+            
+                      //Id
+                      id: "notif"
+                });
+
+                if(this.state.playBeginning) {
+                    this.buttonContainer.current.classList.add("button--container--clicked");
+                } else if (!this.state.playBeginning) {
+                    this.buttonContainer.current.classList.remove("button--container--clicked");
+                }
+            } 
+        }
+
+        this.setState({
+            playBeginning: !this.state.playBeginning
+        })
+         //console.log("Playing audio? " + this.state.playing);
+        //return this.notif();
+    }
+
+    componentDidMount() {
+        if(this.callMethod) {
+            console.log("Changed props: DidMount")
+
+            if(this.callMethod && this.state.counter === 0) {
+                this.beginning();
+                this.setState({
+                    counter: 1
+                });
+                console.log(`button.js ${this.one}`);
+                console.log(`button.js ${this.two}`);
+                console.log(`button.js ${this.three}`);
+            }
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if(this.callMethod !== prevProps.callMethod) {
+            console.log("Changed props: DidUpdate")
+
+            if(this.callMethod && this.state.counter === 0) {
+                this.beginning();
+                this.setState({
+                    counter: 1
+                });
+                console.log(`button.js ${this.one} : DidUpdate`);
+                console.log(`button.js ${this.two} : DidUpdate`);
+                console.log(`button.js ${this.three} : DidUpdate`);
+            }
+        }
     }
 
     render() {
@@ -179,4 +294,7 @@ class Button extends Component {
     }
 }
 
-export default Button;
+const ButtonMemo = React.memo(Button);
+export default ButtonMemo;
+
+//export default Button;
--- src/components/sounds/sounds.js
+++ src/components/sounds/sounds.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from "./button/button";
+import ButtonMemo from "./button/button";
 
 class Sounds extends Component {
 
@@ -9,19 +9,132 @@ class Sounds extends Component {
         //Creating References
         this.container = React.createRef();
         this.body = this.props.body;
-            //References for Sounds
-            this.rain = React.createRef(null);
         
+        //Executing events
         window.addEventListener('scroll', this.styling);
+
+        //State
+        this.state = {
+            callMethod: false,
+
+            firstSound: undefined,
+            secondSound: undefined,
+            thirdSound: undefined
+        }
+
+        //Events
+        this.handleClick = this.handleClick.bind(this);
     }
 
+    //Gives some styles when the page is scrolled at first
     styling = e => {
         e.preventDefault();
 
         if (this.container.current.scrollTop === 0) {
             this.container.current.classList.add("juju")
         }
+    }
 
+    //Extra methods for Random
+        //Generate random number between MIN and MAX
+        numberGenerator = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+        //Deciding which sound to play
+        chooseSound = (num) => {
+            let rest = undefined;
+
+            if (num === 1) {
+                rest = "Rain";
+            } else if (num === 2) {
+                rest = "Library";
+            } else if (num === 3) {
+                rest = "Ocean";
+            } else if (num === 4) {
+                rest = "Campfire";
+            } else if (num === 5) {
+                rest = "Thunder";
+            } else if (num === 6) {
+                rest = "Keyboard";
+            } else if (num === 7) {
+                rest = "Wind";
+            } else if (num === 8) {
+                rest = "Ticking";
+            } else if (num === 9) {
+                rest = "Snow";
+            } else if (num === 10) {
+                rest = "Café";
+            } else if (num === 11) {
+                rest = "Bath";
+            } else if (num === 12) {
+                rest = "Radio";
+            } else if (num === 13) {
+                rest = "River";
+            } else if (num === 14) {
+                rest = "Birds";
+            } else if (num === 15) {
+                rest = "Crowd";
+            } else if (num === 16) {
+                rest = "Trees";
+            }
+
+            return rest;
+        }
+
+    //Play 3 random sounds
+    random = (min, max) => {
+        let firstSound = this.numberGenerator(min, max);
+        let secondSound = this.numberGenerator(min, max);
+        let thirdSound = this.numberGenerator(min, max);
+
+        /*if(firstSound === secondSound || secondSound === thirdSound || thirdSound === firstSound) {
+            firstSound = this.numberGenerator(min, max);
+            secondSound = this.numberGenerator(min, max);
+            thirdSound = this.numberGenerator(min, max);
+        }*/
+
+        console.log(`First sound: "${firstSound}"`);
+        console.log(`Second sound: "${secondSound}"`);
+        console.log(`Third sound: "${thirdSound}"`);
+
+
+        console.log(`. . . . . . . . . . . . . . `);
+
+
+        let first = this.chooseSound(firstSound);
+        let second = this.chooseSound(secondSound);
+        let third = this.chooseSound(thirdSound);
+        first.toString();
+        second.toString();
+        third.toString();
+
+        console.log(`First sound's name: "${first}"`);
+        console.log(`Second sound's name: "${second}"`);
+        console.log(`Third sound's name: "${third}"`);
+
+        console.log(`. . . . . . . . . . . . . . `);
+        
+        this.state.firstSound = first;
+        console.log(`First sound "${this.state.firstSound}" ---  ${firstSound}`);
+        this.state.secondSound = second;
+        console.log(`Second sound "${this.state.secondSound}" ---  ${secondSound}`);
+        this.state.thirdSound = third;
+        console.log(`Third sound "${this.state.thirdSound}" ---  ${thirdSound}`);
+
+        return null;
+    }
+
+    handleClick = e => {
+
+        this.random(1, 16);
+        
+        this.setState({
+            callMethod: true
+        })
+
+        console.log(this.state.callMethod);
+
+        this.container.forceUpdate();
     }
     
     render() {
@@ -35,7 +148,9 @@ class Sounds extends Component {
                     </div>
 
                     <div className = "random">
-                        <button><i className = "fas fa-random"></i></button>
+                        <button onClick = { this.handleClick.bind(this) }>
+                            <i className = "fas fa-random"></i>
+                        </button>
                     </div>
                 </div>
 
@@ -45,74 +160,73 @@ class Sounds extends Component {
 
                     <div className = "sounds--row">
                         <div className = "sounds--icon--container">
-                            <Button name = "Rain" icon = "fas fa-cloud-rain" container = {this.container} audio = { require("./audio/rain.mp3") } ref = { this.rain } />
-                            <button onClick = { this.rain }>joe biden</button>
+                            <ButtonMemo name = "Rain" icon = "fas fa-cloud-rain" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound } method = { this.state.callMethod } audio = { require("./audio/rain.mp3") } />
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Thunder" icon = "fas fa-bolt" container = {this.container} audio = { require("./audio/thunder.mp3") } />
+                            <ButtonMemo name = "Thunder" icon = "fas fa-bolt" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound } method = { this.state.callMethod } audio = { require("./audio/thunder.mp3") } />
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Snow" icon = "fas fa-snowflake" container = {this.container} audio = { require("./audio/snow.mp3") } />
+                            <ButtonMemo name = "Snow" icon = "fas fa-snowflake" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/snow.mp3") } ref = { this.snow } />
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "River" icon = "fas fa-water" container = {this.container} audio = { require("./audio/river-1.mp3") } />
-                        </div>
-                    </div>
-
-                    <div className = "sounds--row">
-                        <div className = "sounds--icon--container">
-                            <Button name = "Library" icon = "fas fa-book" container = {this.container} audio = { undefined } />
-                        </div>
-
-                        <div className = "sounds--icon--container">
-                            <Button name = "Keyboard" icon = "fas fa-keyboard" container = {this.container} audio = { require("./audio/keyboard.mp3") }/>
-                        </div>
-
-                        <div className = "sounds--icon--container">
-                            <Button name = "Café" icon = "fas fa-mug-hot" container = {this.container} audio = { require("./audio/cafe.mp3") }/>
-                        </div>
-
-                        <div className = "sounds--icon--container">
-                            <Button name = "Birds" icon = "fas fa-dove" container = {this.container} audio = { require("./audio/birds.mp3") }/>
+                            <ButtonMemo name = "River" icon = "fas fa-water" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound } method = { this.state.callMethod } audio = { require("./audio/river-1.mp3") } />
                         </div>
                     </div>
 
                     <div className = "sounds--row">
                         <div className = "sounds--icon--container">
-                            <Button name = "Ocean" icon = "fas fa-anchor" container = {this.container} audio = { require("./audio/ocean.mp3") }/>
+                            <ButtonMemo name = "Library" icon = "fas fa-book" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound } method = { this.state.callMethod } audio = { undefined } />
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Wind" icon = "fas fa-wind" container = {this.container} audio = { require("./audio/wind-2.mp3") }/>
+                            <ButtonMemo name = "Keyboard" icon = "fas fa-keyboard" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/keyboard.mp3") }/>
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Bath" icon = "fas fa-bath" container = {this.container} audio = { require("./audio/bath.mp3") }/>
+                            <ButtonMemo name = "Café" icon = "fas fa-mug-hot" container = {this.container} oone = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/cafe.mp3") }/>
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Crowd" icon = "fas fa-users" container = {this.container} audio = { require("./audio/crowd.mp3") }/>
+                            <ButtonMemo name = "Birds" icon = "fas fa-dove" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/birds.mp3") }/>
                         </div>
                     </div>
 
                     <div className = "sounds--row">
                         <div className = "sounds--icon--container">
-                            <Button name = "Campfire" icon = "fas fa-fire" container = {this.container} audio = { require("./audio/campfire.mp3") }/>
+                            <ButtonMemo name = "Ocean" icon = "fas fa-anchor" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/ocean.mp3") }/>
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Ticking" icon = "fas fa-clock" container = {this.container} audio = { require("./audio/ticking.mp3") }/>
+                            <ButtonMemo name = "Wind" icon = "fas fa-wind" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/wind-2.mp3") }/>
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Radio" icon = "fas fa-broadcast-tower" container = {this.container} audio = { require("./audio/radio-2.mp3") }/>
+                            <ButtonMemo name = "Bath" icon = "fas fa-bath" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/bath.mp3") }/>
                         </div>
 
                         <div className = "sounds--icon--container">
-                            <Button name = "Trees" icon = "fas fa-tree" container = {this.container} audio = { require("./audio/forest.mp3") }/>
+                            <ButtonMemo name = "Crowd" icon = "fas fa-users" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/crowd.mp3") }/>
+                        </div>
+                    </div>
+
+                    <div className = "sounds--row">
+                        <div className = "sounds--icon--container">
+                            <ButtonMemo name = "Campfire" icon = "fas fa-fire" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/campfire.mp3") }/>
+                        </div>
+
+                        <div className = "sounds--icon--container">
+                            <ButtonMemo name = "Ticking" icon = "fas fa-clock" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/ticking.mp3") }/>
+                        </div>
+
+                        <div className = "sounds--icon--container">
+                            <ButtonMemo name = "Radio" icon = "fas fa-broadcast-tower" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/radio-2.mp3") }/>
+                        </div>
+
+                        <div className = "sounds--icon--container">
+                            <ButtonMemo name = "Trees" icon = "fas fa-tree" container = {this.container} one = { this.state.firstSound } two = { this.state.secondSound } three = { this.state.thirdSound }method = { this.state.callMethod } audio = { require("./audio/forest.mp3") }/>
                         </div>
                     </div>
 
